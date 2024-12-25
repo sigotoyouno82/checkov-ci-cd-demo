f6392227904573c1dbb0bd3f73e29f1e193c0645
sigotoyouno82 <69370927+sigotoyouno82@users.noreply.github.com>
2024-12-26 01:05:14 +0900

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-northeast-1"
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "example" {
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "example" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
+        sse_algorithm     = "aws:kms"
         kms_master_key_id = "arn:aws:kms:us-east-1:123456789012:key/key-id"
       }
     }
@@ -38,26 +38,6 @@ resource "aws_s3_bucket" "example" {
     }
   }
 
-  replication_configuration {
-    role = "arn:aws:iam::123456789012:role/s3-replication-role"
-
-    rules {
-      id     = "ReplicationRule"
-      status = "Enabled"
-
-      destination {
-        bucket = "arn:aws:s3:::destination-bucket-name"
-      }
-    }
-  }
-
-  notification {
-    topic {
-      topic_arn = "arn:aws:sns:us-east-1:123456789012:example-topic"
-      events    = ["s3:ObjectCreated:*"]
-    }
-  }
-
   tags = {
     Name        = "example-bucket"
     Environment = "dev"
@@ -73,3 +53,15 @@ resource "aws_s3_bucket_public_access_block" "example" {
   restrict_public_buckets = true
 }
 
+resource "aws_s3_bucket_notification" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  lambda_function {
+    lambda_function_arn = "arn:aws:lambda:us-east-1:123456789012:function:example-function"
+    events              = ["s3:ObjectCreated:*"]
+    filter {
+      prefix = "uploads/"
+      suffix = ".jpg"
+    }
+  }
+}
